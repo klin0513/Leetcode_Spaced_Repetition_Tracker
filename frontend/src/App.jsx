28d008df1302dc8b29853f2639dddbbba6478ca7
Kevin Lin <klin0513@terpmail.umd.edu>
2026-06-20 10:30:00 -0400

    added routes for register and login pages; added signup/login btns to nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,24 +13,36 @@ function App() {
         style={{
           position: "sticky",
           top: "0",
-          padding: "10px",
+          padding: "0.9375rem 2rem 0.9375rem 10rem",
           background: "#f8f9fa",
           display: "flex",
-          gap: "15px",
         }}
       >
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/dashboard" className="nav-link">
-          Dashboard
-        </Link>
+        <div style={{ display: "flex", gap: "25px" }}>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/dashboard" className="nav-link">
+            Dashboard
+          </Link>
+        </div>
+
+        <div style={{ display: "flex", gap: "15px", marginLeft: "auto" }}>
+          <Link to="/login" className="nav-link nav-btn nav-btn-login">
+            Log In
+          </Link>
+          <Link to="/register" className="nav-link nav-btn nav-btn-signup">
+            Sign Up
+          </Link>
+        </div>
       </nav>
 
       {/* routes to swap pages */}
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
