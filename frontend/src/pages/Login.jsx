2456b28152ea84d9685fa6a22acf91fc5c0f78cc
Kevin Lin <klin0513@terpmail.umd.edu>
2026-06-28 10:23:57 -0400

    fixed invalid DOM property error and removed unused assets

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -33,10 +33,10 @@ export default function Login() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-mail-icon lucide-mail"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-mail-icon lucide-mail"
             >
               <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
               <rect x="2" y="4" width="20" height="16" rx="2" />
@@ -59,10 +59,10 @@ export default function Login() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-lock-icon lucide-lock"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-lock-icon lucide-lock"
             >
               <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
               <path d="M7 11V7a5 5 0 0 1 10 0v4" />
